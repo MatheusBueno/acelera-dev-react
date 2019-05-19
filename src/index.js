43eb06cf4614fa7ad7192a1467565244274bda85
Douglas Santos <drs.90@hotmail.com>
2019-05-18 22:58:09 -0300

    global styles

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Router from './routes';
+import Router from "./routes";
+import "./global.css";
 
 ReactDOM.render(<Router />, document.getElementById("root"));
