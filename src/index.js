65e5c245c3a4d674f4a2ef634c455cadfbb686a2
Douglas Santos <drs.90@hotmail.com>
2019-05-18 23:21:12 -0300

    add reset-css package

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Router from "./routes";
+import 'reset-css'
 import "./global.css";
 
 ReactDOM.render(<Router />, document.getElementById("root"));
