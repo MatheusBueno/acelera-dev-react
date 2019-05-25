20ae365e421020147cc57c50359484f1b2b6c1d5
Douglas Santos <drs.90@hotmail.com>
2019-05-25 14:03:00 -0300

    removed hooks and organized props

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,23 +1,43 @@
-import React, { useState } from "react";
-
+import React from "react";
+import PropTypes from "prop-types";
 import HeaderContainer from "./styles";
-import {InputSearch} from "../Input";
+import { InputSearch } from "../Input";
 import Button from "../Button";
 
-const Header = ({ handleInput, input }) => {
-  const [isFull, setFull] = useState(true);
-
-  const search = () => setFull(!isFull);
+const Header = ({
+  inputSearchValue,
+  handleInput,
+  enterPressCheck,
+  handleSearch,
+  isFull
+}) => {
   return (
-    <HeaderContainer full={!isFull}>
+    <HeaderContainer full={isFull}>
       <InputSearch
-        handleInput={handleInput}
-        value={input}
+        onChange={handleInput}
+        onKeyPress={enterPressCheck}
+        value={inputSearchValue}
         placeholder="Find a user"
       />
-      <Button onClick={search}>Search</Button>
+      <Button onClick={handleSearch}>Search</Button>
     </HeaderContainer>
   );
+};
+
+Header.defaultProps = {
+  inputSearchValue: "",
+  isFull: true,
+  handleInput: function() {},
+  enterPressCheck: function() {},
+  handleSearch: function() {}
+};
+
+Header.propTypes = {
+  inputSearchValue: PropTypes.string,
+  isFull: PropTypes.bool,
+  handleInput: PropTypes.func,
+  enterPressCheck: PropTypes.func,
+  handleSearch: PropTypes.func
 };
 
 export default Header;
