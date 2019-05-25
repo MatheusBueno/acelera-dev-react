94b987d59d75915d126801db6c2b081c2880d465
Douglas Santos <drs.90@hotmail.com>
2019-05-25 14:07:26 -0300

    organized props of InputSearch

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 import styled from "styled-components";
 import { Container, SearchIcon } from "./styles";
 
@@ -24,13 +25,32 @@ const Input = styled.input`
   }
 `;
 
-export const InputSearch = ({ input, handleInput }) => {
+export const InputSearch = ({ value, onInput, onKeyPress, onChange }) => {
   return (
     <Container>
-      <Input type="search" value={input} onChange={handleInput} />
+      <Input
+        type="search"
+        value={value}
+        onInput={onInput}
+        onChange={onChange}
+        onKeyPress={onKeyPress}
+      />
       <SearchIcon />
     </Container>
   );
+};
+
+InputSearch.defaultProps = {
+  value: "",
+  onInput: function() {},
+  onChange: function() {},
+  onKeyPress: function() {}
+};
+InputSearch.propTypes = {
+  value: PropTypes.string,
+  onInput: PropTypes.func,
+  onChange: PropTypes.func,
+  onKeyPress: PropTypes.func
 };
 
 export default Input;
