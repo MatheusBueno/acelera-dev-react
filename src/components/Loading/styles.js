8ae74c77984c6a588a958792ba72c90693a3af5c
Douglas Santos <drs.90@hotmail.com>
2019-05-26 20:45:28 -0300

    wip loading

---
+++ src/components/Loading/styles.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const Styles = styled.div`
+  background: #000;
+  opacity: 0.5;
+  width: 100vw;
+  height: 100vh;
+  z-index: 10;
+  position: absolute;
+`;
+
+export default Styles;
