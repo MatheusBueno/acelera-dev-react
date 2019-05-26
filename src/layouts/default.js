7a951a4915e3adf6fe55c3703cbd15cb2df0f7ac
Douglas Santos <drs.90@hotmail.com>
2019-05-26 20:47:23 -0300

    added header props and fuctions

--- src/layouts/default.js
+++ src/layouts/default.js
@@ -1,36 +1,62 @@
 import React, { useState } from "react";
-
+import { connect } from "react-redux";
+import actions from "../states/users/actions";
 import { Route } from "react-router-dom";
 import Header from "../components/Header";
-import RepositoryService from "../services/repository";
+import Loading from "../components/Loading";
 
-const DefaultLayout = ({ component: Component, ...rest }) => {
+const DefaultLayout = ({
+  component: Component,
+  isLoading,
+  users,
+  searchUser,
+  ...rest
+}) => {
   const [input, setInput] = useState("");
-
   const handleInput = e => {
     const { value } = e.target;
     setInput(value);
-
-    RepositoryService.searchUsers({ username: value })
-      .then(response => {
-        console.log(response);
-      })
-      .catch(error => {
-        console.error(error);
-      });
+  };
+  const enterPressCheck = e => {
+    return e.key === "Enter" && handleSearch();
+  };
+  const handleSearch = () => {
+    searchUser({ query: input });
   };
 
   return (
-    <Route
-      {...rest}
-      render={matchProps => (
-        <div className="DefaultLayout">
-          <Header handleInput={handleInput} value={input} />
-          <Component {...matchProps} />
-        </div>
-      )}
-    />
+    <>
+      <Header
+        inputSearchValue={input}
+        handleInput={handleInput}
+        enterPressCheck={enterPressCheck}
+        handleSearch={handleSearch}
+        isFull={users && users.length === 0}
+      />
+      <Route
+        {...rest}
+        render={matchProps => (
+          <div className="DefaultLayout">
+            {isLoading && <Loading />}
+            <Component {...matchProps} />
+          </div>
+        )}
+      />
+    </>
   );
 };
 
-export default DefaultLayout;
+const mapDispatchToProps = dispatch => {
+  return {
+    searchUser: payload => dispatch(actions.fetchUsers(payload))
+  };
+};
+const mapStateToProps = state => ({
+  error: state.error,
+  users: state.users.users,
+  isLoading: state.users.isLoading
+});
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(DefaultLayout);
